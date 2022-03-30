9544c4d5dad1854d77da776843e8aa7396108b24
George <70286427+GeorgeTorres-py@users.noreply.github.com>
2022-03-29 23:35:21 -0400

    Create state_variables.sol

---
+++ state_variables.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+//State variables are variables whose values are permanently stored in contract storage.
+
+
+pragma solidity >=0.4.0 <0.9.0;
+
+contract SimpleStorage {
+    uint storedData; // State variable
+    // ...
+}

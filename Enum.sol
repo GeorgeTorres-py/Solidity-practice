9065d9a7596f1585e27e29d298358279cfe25188
George <70286427+GeorgeTorres-py@users.noreply.github.com>
2022-03-31 22:53:22 -0700

    Enum.sol

---
+++ Enum.sol
@@ -0,0 +1,21 @@
+//Solidity-Identifier-License GPL: 3.0
+
+
+//Enums are one way to create 
+//a user-defined type in Solidity
+
+//Using type(NameOfEnum).min and type(NameOfEnum).max 
+//you can get the smallest and respectively 
+//largest value of the given enum.
+
+pragma solidity >=0.4.0 <0.9.0;
+
+
+contract Purchase {
+    enum State { Created, Locked, Insctive}
+
+}
+
+
+
+

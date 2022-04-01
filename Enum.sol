8bbec8419677f45547ba3b1e661c81dec8dc9163
George <70286427+GeorgeTorres-py@users.noreply.github.com>
2022-03-31 22:58:39 -0700

    Update Enum.sol

--- Enum.sol
+++ Enum.sol
@@ -4,9 +4,6 @@
 //Enums are one way to create 
 //a user-defined type in Solidity
 
-//Using type(NameOfEnum).min and type(NameOfEnum).max 
-//you can get the smallest and respectively 
-//largest value of the given enum.
 
 pragma solidity >=0.4.0 <0.9.0;
 
@@ -14,6 +11,17 @@ pragma solidity >=0.4.0 <0.9.0;
 contract Purchase {
     enum State { Created, Locked, Inactive}
 
+//Using type(NameOfEnum).min and type(NameOfEnum).max 
+//you can get the smallest and respectively 
+//largest value of the given enum.
+
+    function getLargestValue() public pure returns (Purchase) {
+        return type(Purchase).max;
+    }
+
+    function getSmallestValue() public pure returns (Purchase) {
+        return type(Purchace).min;
+    }
 }
 
 

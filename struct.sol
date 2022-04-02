1c6d8ebda0b59083d600716642cea28ea7417e41
George <70286427+GeorgeTorres-py@users.noreply.github.com>
2022-04-01 19:15:14 -0700

    struct

---
+++ struct.sol
@@ -0,0 +1,13 @@
+//Structs are custom defined types that can group several variables
+pragma solidity >=0.4.0 <0.9.0;
+
+contract Ballot
+    struck Voter {
+      uint weight;
+      bool voted;
+      address delegated;
+      uint vote;
+    }
+}
+
+
